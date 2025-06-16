e39d18a09d4cd481baa6e5078b50038488dfa0b0
Tanzil Hussain <tanzilhannah@gmail.com>
2025-06-15 22:17:18 -0700

    new additions

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+## import tiktoken 
 
 # set up web server
 app = Flask (__name__)
 
 ## homepage
+@app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/count', methods =['POST'])
+def countTokens():
+    data = prompt.get_json()
+    prompt = data.get('prompt','')
+    token_count = len(prompt.split())
+    return jsonify({'token_count': token_count})
